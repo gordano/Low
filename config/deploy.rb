de09345b94d3e06d988d14fa5e0b8f46e56ba1f0
= <=>
2016-03-18 11:06:41 -0700

    Cap settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,8 +8,9 @@ set :repo_url, 'git@github.com:gordano/low.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-  set :rbenv_path, '/home/deploy/.rbenv'
-  set :deploy_to,  '/home/deploy/lowohm'
+set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
+set :rbenv_path, '/home/deploy/.rbenv'
+set :deploy_to,  '/home/deploy/lowohm'
 
 # Default value for :scm is :git
 # set :scm, :git
