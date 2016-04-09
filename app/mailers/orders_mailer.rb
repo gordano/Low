55314991e8e50eb51f3bf74396aeb67f4d5827f9
= <=>
2016-04-09 07:50:03 -0700

    Heroku mails from Shop

--- app/mailers/orders_mailer.rb
+++ app/mailers/orders_mailer.rb
@@ -1,13 +1,14 @@
 class OrdersMailer < ApplicationMailer
-   default from: "headbat@gmail.com",
+  default from: "headbat@gmail.com",
   template_path: 'mailers/orders'
 
   def order_placed(order)
     @order = order
-    mail to: ['headbat@gmail.com, headbat@yandex.ru'],
-         from: 'LowOhm<mess-tver@yandex.ru>',
-         #subject: @test.user.name + ': Тест пройден.'
-         subject: 'Заказ №'+ @order.id.to_s + ', оформил: '+@order.fio
+    mails = Shop.mail(Shop.last.id)
+    mails ||= 'headbat@gmail.com'
 
+    mail to: [mails],
+         from: 'LowOhm<mess-tver@yandex.ru>',
+         subject: 'Заказ №'+ @order.id.to_s + ', оформил: '+@order.fio
   end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,7 +1,11 @@
 class Shop < ActiveRecord::Base
   validates_presence_of :email, :title, :about, :phone,
                         :presence => true
-  validates_format_of   :email,
-                        :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  #validates_format_of   :email,
+  #                      :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates_numericality_of :phone, only_integer: true
+
+  def self.mail(id)
+    find_by_id(id) ? find_by_id(id).email : nil
+  end
 end
